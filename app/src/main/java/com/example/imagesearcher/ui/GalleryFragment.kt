e1fedb5a28e2514d7a2b87cb56a2e421b7bac966
Krzysztof Kocot <kkocot2@dxc.com>
2020-10-09 14:03:49 +0200

    Search from menu

--- app/src/main/java/com/example/imagesearcher/ui/GalleryFragment.kt
+++ app/src/main/java/com/example/imagesearcher/ui/GalleryFragment.kt
@@ -1,13 +1,19 @@
 package com.example.imagesearcher.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.imagesearcher.PixbayViewModel
 import com.example.imagesearcher.R
 import com.example.imagesearcher.databinding.FragmentGalleryBinding
+import com.jakewharton.rxbinding4.appcompat.queryTextChanges
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
@@ -15,6 +21,7 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     private val viewModel by viewModels<PixbayViewModel>()
     private var _binding: FragmentGalleryBinding? = null
     private val binding get() = _binding!!
+    private var disposable = CompositeDisposable()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -40,10 +47,34 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
             .subscribe(viewLifecycleOwner) {
                 adapter.retry()
             }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        disposable.add(
+            searchView.queryTextChanges()
+                .skipInitialValue()
+                .debounce(700L, TimeUnit.MILLISECONDS)
+                .distinctUntilChanged()
+                .filter { it.length >= 3 }
+                .subscribeAndroid {
+                    if (it.isNotBlank()) {
+                        binding.recyclerView.scrollToPosition(0)
+                        viewModel.searchPhotos(it.toString())
+                    }
+                })
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        disposable.clear()
     }
 }
--- app/src/main/java/com/example/imagesearcher/ui/Utils.kt
+++
@@ -1,8 +0,0 @@
-package com.example.imagesearcher.ui
-
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
-
-fun <T> LiveData<T>.subscribe(lifecycleOwner: LifecycleOwner, call: (T) -> Unit) =
-    observe(lifecycleOwner, Observer { call(value!!) })
